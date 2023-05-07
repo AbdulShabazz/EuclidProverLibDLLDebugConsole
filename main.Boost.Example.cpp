53e66d2ee59782d1f3ef8e37effc674b1188db2e
AbdulShabazz <AbdulShabazz@users.noreply.github.com>
2023-05-07 12:14:05 -0400

    Update main.Boost.Example.cpp

--- main.Boost.Example.cpp
+++ main.Boost.Example.cpp
@@ -10,16 +10,33 @@
 #include <deque>
 #include <string>
 #include <unordered_map>
-#include <boost/multiprecision/cpp_int.hpp>
+#include <_boost/multiprecision/cpp_int.hpp>
 
 int main()
 {
-    const auto start_time_chrono = std::chrono::high_resolution_clock::now();
+	const auto start_time_chrono = std::chrono::high_resolution_clock::now();
 
-	boost::multiprecision::cpp_int PrimeComposite = 4;
-	boost::multiprecision::cpp_int PrimeComposite2 = 2; 
-	boost::multiprecision::cpp_int PrimeComposite3 = 16;
+	boost::multiprecision::cpp_int PrimeComposite = 12345678901234567890;
+	boost::multiprecision::cpp_int PrimeComposite2 = 12345678901234567890;
+	boost::multiprecision::cpp_int PrimeComposite3 = 12345678901234567890;
 	boost::multiprecision::cpp_int PrimeComposite_Result;
+
+	++PrimeComposite2;
+	PrimeComposite2--;
+	PrimeComposite2++;
+	--PrimeComposite2;
+
+	PrimeComposite_Result = PrimeComposite2 + PrimeComposite3 - PrimeComposite3;
+
+	PrimeComposite_Result += PrimeComposite2;
+
+	PrimeComposite_Result -= PrimeComposite2;
+
+	PrimeComposite_Result *= PrimeComposite2;
+
+	std::cout << "PrimeComposite_Result (BigInt): " << PrimeComposite_Result << std::endl;
+
+	PrimeComposite_Result /= PrimeComposite2;
 
 	if (PrimeComposite % PrimeComposite2 == 0)
 	{
@@ -28,9 +45,9 @@ int main()
 
 	const auto end_time_chrono = std::chrono::high_resolution_clock::now();
 	const auto duration_chrono = std::chrono::duration_cast<std::chrono::nanoseconds>(end_time_chrono - start_time_chrono).count();
-
+	
 	std::cout << "PrimeComposite_Result (BigInt): " << PrimeComposite_Result << std::endl;
 	std::cout << "Total Duration (nanoseconds): " << duration_chrono << std::endl;
 
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
