b5f8877ef381441bfaa65c0fe64186fd6630aac1
AbdulShabazz <AbdulShabazz@users.noreply.github.com>
2023-05-06 23:41:55 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,16 +10,16 @@
 #include <deque>
 #include <string>
 #include <unordered_map>
-#include "../boost/multiprecision/cpp_int.hpp"
+#include <boost/multiprecision/cpp_int.hpp>
 
 int main()
 {
     const auto start_time_chrono = std::chrono::high_resolution_clock::now();
 
-	cpp_int PrimeComposite = 4; 
-	cpp_int PrimeComposite2 = 2; 
-	cpp_int PrimeComposite3 = 16;
-	cpp_int PrimeComposite_Result;
+	boost::multiprecision::cpp_int PrimeComposite = 4;
+	boost::multiprecision::cpp_int PrimeComposite2 = 2; 
+	boost::multiprecision::cpp_int PrimeComposite3 = 16;
+	boost::multiprecision::cpp_int PrimeComposite_Result;
 
 	if (PrimeComposite % PrimeComposite2 == 0)
 	{
@@ -28,6 +28,8 @@ int main()
 
 	const auto end_time_chrono = std::chrono::high_resolution_clock::now();
 	const auto duration_chrono = std::chrono::duration_cast<std::chrono::nanoseconds>(end_time_chrono - start_time_chrono).count();
+
+	std::cout << "PrimeComposite_Result (BigInt): " << PrimeComposite_Result << std::endl;
 	std::cout << "Total Duration (nanoseconds): " << duration_chrono << std::endl;
 
     return EXIT_SUCCESS;
