9098400474a6ef2a1d5759d236f08c4cfdb87e79
AbdulShabazz <AbdulShabazz@users.noreply.github.com>
2023-05-08 14:58:00 -0400

    Update AddEuclidProverLibDLL.bat

--- AddEuclidProverLibDLL.bat
+++ AddEuclidProverLibDLL.bat
@@ -1,1 +1,4 @@
 xcopy /Y "C:\Users\ahsha\Documents\Visual Studio 2022\EuclidProverLibDLL\x64\Release\EuclidProverLibDLL.dll" "x64\Release"
+xcopy /Y "C:\Users\ahsha\Documents\Visual Studio 2022\EuclidProverLibDLL\x64\Release\EuclidProverLibDLL.lib" "x64\Release"
+xcopy /Y "C:\Users\ahsha\Documents\Visual Studio 2022\EuclidProverLibDLL\x64\Release\EuclidProverLibDLL.lib" ".\"
+xcopy /Y "C:\Users\ahsha\Documents\Visual Studio 2022\EuclidProverLibDLL\Euclid.h" ".\"
